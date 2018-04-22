dc0ae6f2a8555d1224e9256243271f6c3ab116be
Maher-Abedah <m.abedah1@gmail.com>
2018-04-22 18:18:43 +0300

    adding README file

--- README.md
+++ README.md
@@ -1,20 +1,21 @@
 # Memory Game Project
-
+ this project was made as a part of the Front-End Nanodegree course by Udacity.
+ 
 ## Table of Contents
 
-* [Instructions](#instructions)
-* [Contributing](#contributing)
+* [Installation](#Installation)
+* [Instructions](#Instructions)
+
+## Installation
+
+Clone the repository to your local device, start by launching the index.html file   
+
+or you can use the online version on the folowing link:
+
 
 ## Instructions
 
-The starter project has some HTML and CSS styling to display a static version of the Memory Game project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
-
-To get started, open `js/app.js` and start building out the app's functionality
-
-For specific, detailed instructions, look at the project instructions in the [Udacity Classroom](https://classroom.udacity.com/me).
-
-## Contributing
-
-This repository is the starter code for _all_ Udacity students. Therefore, we most likely will not accept pull requests.
-
-For details, check out [CONTRIBUTING.md](CONTRIBUTING.md).
+you need to fined the matched cards in the least number of moves,
+for 26 moves or less you get a Three-star rating,
+from 27 to 33 moves you get two stars,
+and for more than 34 moves you get one star rating. 
--- js/app.js
+++ js/app.js
@@ -1,10 +1,8 @@
-
- let seconds =0,minutes = 0,interval;
-
+//declaring variables.
 let moves = document.querySelector('.moves');
 let cardsDeck = document.querySelector('.deck'); 
 let cards = document.querySelectorAll('.card'); 
-let cardsArray = [...cards];
+let cardsArray = [...cards]; //iterating [cards] so we can use it as an array
 const repeatButton = document.querySelector('.restart');
 let cardList = ["fa fa-diamond", "fa fa-paper-plane-o", "fa fa-anchor",
 			    " fa fa-bolt", "fa fa-cube", "fa fa-leaf",
@@ -13,14 +11,10 @@ let cardList = ["fa fa-diamond", "fa fa-paper-plane-o", "fa fa-anchor",
 			    "fa fa-leaf", "fa fa-bicycle", "fa fa-bomb"];
 let matchedCards = [];
 let openedCards =[];
-let li = document.getElementsByTagName('li');
 let movesCount = 0;
-let targetClass = "";
-let starsElem = document.querySelector('.stars');
 let starsNo = 3;
 
-
-console.log('strs Element is: ' + starsElem.parentElement.innerHTML);
+//initiate the game 
 shuffle(cardList);
 for (let i = 0; i<cardsArray.length;i++){
 	cardsArray[i].innerHTML = '<i></i>';
@@ -30,7 +24,6 @@ for (let i = 0; i<cardsArray.length;i++){
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
     let currentIndex = array.length, temporaryValue, randomIndex;
-    console.log("current index is: " + currentIndex)
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex -= 1;
@@ -41,43 +34,44 @@ function shuffle(array) {
     return array;
 }
 
-
+// to display the content of the card
 function displayCard(){
 	event.target.classList.add('open');
 };
 
-
+// I included more than one functionality in this one,
+// I'll write comments for each one
 function matchCard(){
 	let openedCardsNo = openedCards.length;
 	let matchedCardsNo = matchedCards.length;
 	let targetList = event.target;
-	if(targetList.nodeName === 'LI'){
+	if(targetList.nodeName === 'LI'){ //To make sure clicks outside the card are not counted
 		if (openedCards.length === 0 && targetList.className === 'card open'){
-			openedCards[0] = targetList;
+			openedCards[0] = targetList;//filling the openedCards array with first clicked element
 		}else if(openedCards.length === 1 && targetList.className === 'card open' && targetList!== openedCards[0]){
-			openedCards[1] = targetList;
+			openedCards[1] = targetList;//adding the second element to the array after confirming the click was on another card
 		}
 			
 		if (openedCards.length === 2){
 			for (const openCard of openedCards){
 				let openedCard1 = openedCards[0];
 				let openedCard2 = openedCards[1];
-				console.log('opened cards1 is: ' + openedCard1.innerHTML);
-				console.log('opened cards2 is: ' + openedCard2.innerHTML);
-	
+	    //adding the match class if they do match 
 				if(openedCard1.firstElementChild.className === openedCard2.firstElementChild.className){
 					openedCard1.className += ' match';
 					openedCard2.className += ' match';
 					matchedCards.length = matchedCards.length + 2;
 					openedCards.length = 0;
-					console.log('matched cards = ' + matchedCards.length);
+					//The condition to win the game
 					if (matchedCards.length === 16){
+						//Added some delay so the user can see the last card flip
 						setTimeout(function(){
 					 		gameWin();
 						 }, 1500);
 						
 					} 
 				}else{
+					//Adding the not-matched class foe 1 second and closing the card after that
 					 setTimeout(function(){
 					 openedCard1.className = 'card';
 					 openedCard2.className = 'card';
@@ -92,19 +86,19 @@ function matchCard(){
 		countMoves();
 	}
 }
-
+// To count the moves
 function countMoves (){
 	movesCount++;
 	moves.textContent = (movesCount);
 }
-
+// When winning the game 
 function gameWin(){
 	let gameFinish = document.querySelector('.no-win');
 	let container = document.querySelector('.container');
 	let finalMoves = document.querySelector('#countNumber');
 	let finalRating = document.querySelector('#finalStars');
 	let restartButton = document.querySelector('#restartButton');
-
+//I choosed to have the final results div in the HTML but be hidden until the condition is fulfilled
 	gameFinish.classList.toggle('no-win');
 	gameFinish.classList.toggle('win');
 	container.style.display = 'none';
@@ -144,13 +138,3 @@ cardsDeck.addEventListener('click', function(event){
 	matchCard(event);
 	rating();
 });
-
-
-function delay(milliseconds) {
-  let start = new Date().getTime();
-  for (let i = 0; i < 1e7; i++) {
-    if ((new Date().getTime() - start) > milliseconds){
-      break;
-    }
-  }
-};
